b64b8861bdd176cf1b96661a484da33359443f4f
Evigila <evigila.shangyi@gmail.com>
2026-05-25 22:40:30 -0300

    Feat: MainWindow ui design conception

---
+++ Vistara/Views/EditorPage.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml.Controls;
+
+namespace Vistara.Views;
+
+public sealed partial class EditorPage : Page
+{
+    public EditorPage()
+    {
+        InitializeComponent();
+    }
+}
--- Vistara/Views/MainWindow.xaml.cs
+++ Vistara/Views/MainWindow.xaml.cs
@@ -1,31 +1,79 @@
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace Vistara.Views;
 
-namespace Vistara.Views
+public sealed partial class MainWindow : Window
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+        Title = "Vistara";
+        ExtendsContentIntoTitleBar = true;
+        SetTitleBar(AppTitleBar);
+
+        RootFrame.Navigated += RootFrame_Navigated;
+        RootNavigationView.SelectedItem = HomeItem;
+        NavigateToTag("Home", false);
+    }
+
+    private void TitleBar_BackRequested(TitleBar sender, object args)
+    {
+        if (RootFrame.CanGoBack)
         {
-            InitializeComponent();
+            RootFrame.GoBack();
+        }
+    }
+
+    private void TitleBar_PaneToggleRequested(TitleBar sender, object args)
+    {
+        RootNavigationView.IsPaneOpen = !RootNavigationView.IsPaneOpen;
+    }
+
+    private void RootNavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+    {
+        if (args.SelectedItemContainer?.Tag is string tag)
+        {
+            NavigateToTag(tag, true);
+        }
+    }
+
+    private void RootFrame_Navigated(object sender, NavigationEventArgs e)
+    {
+        AppTitleBar.IsBackButtonEnabled = RootFrame.CanGoBack;
+        RootNavigationView.SelectedItem = e.SourcePageType.Name switch
+        {
+            nameof(HomePage) => HomeItem,
+            nameof(GalleryPage) => GalleryItem,
+            nameof(EditorPage) => EditorItem,
+            nameof(SettingsPage) => SettingsItem,
+            _ => HomeItem,
+        };
+    }
+
+    private void NavigateToTag(string tag, bool addToBackStack)
+    {
+        var targetPage = tag switch
+        {
+            "Home" => typeof(HomePage),
+            "Gallery" => typeof(GalleryPage),
+            "Editor" => typeof(EditorPage),
+            "Settings" => typeof(SettingsPage),
+            _ => typeof(HomePage),
+        };
+
+        if (RootFrame.CurrentSourcePageType == targetPage)
+        {
+            return;
+        }
+
+        RootFrame.Navigate(targetPage);
+
+        if (!addToBackStack && RootFrame.CanGoBack)
+        {
+            RootFrame.BackStack.Clear();
         }
     }
 }
